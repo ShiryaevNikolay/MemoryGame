bff471a43e542146d04c08bb00f5d79d01980eb8
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-26 19:02:38 +0300

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by shiryaev on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
