88700b36192b169b7ec3a57bd0860372e5801e5c
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-10-03 11:25:51 +0300

    Генерация карт

--- MemoryGame/EmojiMemoryGame.swift
+++ MemoryGame/EmojiMemoryGame.swift
@@ -9,10 +9,26 @@ import Foundation
 
 // ViewModel
 class EmojiMemoryGame {
+    // Иммет смысл написать typealias для MemoryGame<String>
     
-    init(model: MemoryGame<String>) {
-        self.model = model
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👾", "🚕"]
+        return MemoryGame<String>(numberOfPairOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
     }
     
-    var model: MemoryGame<String>
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards // если одна строчка, то можно убрать return
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
 }
